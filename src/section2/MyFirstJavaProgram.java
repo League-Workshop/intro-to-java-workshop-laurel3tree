99b937b113561a9b877be1d326259de7b89ff994
League Student <league@iMac-8.attlocal.net>
2019-07-16 14:29:02 -0700

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -5,9 +5,28 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		// START HERE
+	Robot rascal = new Robot();
+
+rascal.penDown();
+rascal.setPenColor(Color.blue);
+rascal.setPenWidth(5);
+
+rascal.move(100);
+rascal.turn(90);
+rascal.move(100);
+rascal.turn(90);
+rascal.move(100);
+rascal.turn(90);
+rascal.move(100);
+
+
+
+
+	
+	
 	
 		
 		
--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -1,20 +1,26 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
+Robot rascal = new Robot("batman");
 
         // 3. Put the robot's pen down
-
+rascal.penDown();
+rascal.setPenColor(Color.green);
 
         // 6. Make the robot move as fast as possible
-
+rascal.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
+for (int i = 0; i < 360; i++) {
+	rascal.move(2);
+	rascal.turn(360/360);
 
 
         //         2. Move your robot 200 pixels
@@ -22,7 +28,7 @@ public class SmartShapes {
 
         //         4. Turn the robot 90 degrees to the right
 
-    	
+}
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
    
     }
