ea77538d26fa5798d64ecda981918b1c7673e66b
League Student <league@iMac-9.attlocal.net>
2019-07-17 14:29:32 -0700

    Day 3

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,41 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	int random = new Random().nextInt(4);
 	// 3. Print out this variable
-
+	JOptionPane.showMessageDialog(null, random);
 	// 4. Get the user to enter something that they think is awesome
-
+String question = JOptionPane.showInputDialog("Enter an awesome number.");
 	// 5. If the random number is 0
+if(random == 0) {
 
 	// -- tell the user whatever they entered is awesome!
-
+JOptionPane.showMessageDialog(null, "Awesome!");
+}
 	// 6. If the random number is 1
-
+else if(random == 1) {
 	// -- tell the user whatever they entered is ok.
-
+	JOptionPane.showMessageDialog(null, "Ok");
+}
 	// 7. If the random number is 2
-
+else if(random == 2) {
 	// -- tell the user whatever they entered is boring.
-
+	JOptionPane.showMessageDialog(null, "Bo-ring!");
+}
 	// 8. If the random number is 3
-
+else if(random == 3) {
 	// -- write your own answer
-
+	JOptionPane.showMessageDialog(null, "Fabulous");
+}
+}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String question = JOptionPane.showInputDialog("Does the earth revolve around the sun?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(question.equals("yes")) {
+			score = score + 1;
+		}
+		question = JOptionPane.showInputDialog("Does 3 + 3 = 6?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(question.equals("yes")) {
+			score = score + 1;
+		}
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
+		JOptionPane.showMessageDialog(null, score);
 		
 	}
 }
